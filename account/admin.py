ea400cbd39fc615a91930ada8b19cb7755afac76
chris0076 <crcoll9955@northgeorgia.edu>
2012-10-26 23:21:14 -0400

    Add UserProfile information to admin page

---
+++ account/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+from account.models import UserProfile
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = "profile"
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
