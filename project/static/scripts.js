f5f8426deace576c474edcde22e61e98549addef
chris0076 <crcoll9955@northgeorgia.edu>
2012-11-21 21:19:44 -0500

    Do not hide form if there is an error

--- project/static/scripts.js
+++ project/static/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-    $(".hide").hide()
+    if ($("ul.errorlist").length == 0) {
+        $(".hide").hide();
+    }
     $(".hide-title").click(function() {
         $(this).next(".hide").slideToggle("fast");
     });
