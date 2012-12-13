0f301468530c02c9166eac4bc70b16664ba451e3
chris0076 <crcoll9955@northgeorgia.edu>
2012-12-12 20:45:33 -0500

    Move login, logout, register, and active urls to base path
    This is done to clean out the /u/ namespace so that all usernames
    are possible.

--- account/urls.py
+++ account/urls.py
@@ -5,11 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('account.views',
     url(r'^$', "change_settings", name="user_settings"),
-    url(r"^login/$", "login_user", name="login"),
-    url(r"^logout/$", "logout_user", name="logout"),
-    url(r"^register/$", "register_user", name="register"),
-    url(r"^register/(?P<activation_key>[a-f0-9]*)$", "activate_user", name="activate"),
     url(r"^genkey/$", "generate_key", name="generate_key"),
     url(r"^(?P<username>[\w.@+-]+)/id_rsa.pub$", "get_public_key", name="get_public_key"),
 )
-
--- project/urls.py
+++ project/urls.py
@@ -8,3 +8,10 @@ urlpatterns = patterns('',
     url(r'^chem/', include("chem.urls")),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('account.views',
+    url(r"^login/$", "login_user", name="login"),
+    url(r"^logout/$", "logout_user", name="logout"),
+    url(r"^register/$", "register_user", name="register"),
+    url(r"^register/(?P<activation_key>[a-f0-9]*)$", "activate_user", name="activate"),
+)
