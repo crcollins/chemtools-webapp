f0641a1d41a88dcd40213f766511d7f381a20abc
chris0076 <crcoll9955@northgeorgia.edu>
2013-01-26 21:18:10 -0500

    Convert file parsing to be even more like a plugin system

--- chem/fileparser.py
+++ chem/fileparser.py
@@ -15,221 +15,18 @@ np.seterr(all="ignore")
 # Parsers
 ##############################################################################
 
-
-def is_done(fn):
-    def wrapper(self, *args, **kwargs):
-        if not self.done:
-            return fn(self, *args, **kwargs)
-        else:
-            return self.value
-    return wrapper
-
-class Parser(object):
-    def __init__(self):
-        self.done = False
-        self.value = None
-        self.range = (0, 0)
-        self.newfile = False
-    def parse(self, line):
-        raise NotImplementedError
-    def __str__(self):
-        return str(self.value)
-    # def header(self):
-    #     return " ".join(re.split("([A-Z][^A-Z]*)", self.__class__.__name__)[1::2])
-
-##############################################################################
-# Parsers
-##############################################################################
-
-class HomoOrbital(Parser):
-    def __init__(self):
-        super(HomoOrbital, self).__init__()
-        self.value = 0
-        self.range = (-1000, -1)
-
-    @is_done
-    def parse(self, line):
-        # " Alpha  occ. eigenvalues --  -88.90267 -19.16896 -19.16575 -19.15705 -19.15234"
-        # " Alpha  occ. eigenvalues --  -10.23876 -10.23843 -10.23775 -10.23715 -10.23374"
-        # ...
-        # " Alpha virt. eigenvalues --   -0.00138   0.03643   0.07104   0.08148   0.08460"
-        if "Alpha  occ. eigenvalues" in line:
-            self.value += len(line.split()[4:])
-        elif self.value:
-            self.value = str(self.value)
-            self.done = True
-
-
-class Energy(Parser):
-    def __init__(self):
-        super(Energy, self).__init__()
-        self.start = False
-        self.range = (-30, -1)
-
-    @is_done
-    def parse(self, line):
-        # " 36\\Version=EM64L-G09RevC.01\State=1-A\HF=-1127.8085512\RMSD=3.531e-09"
-        if "HF=" in line:
-            idx = line.index("HF=") + 3
-            self.value = line[idx:].split("\\")[0].strip()
-            self.start = True
-            if "\\" in line[idx:]:
-                self.done = True
-        elif self.start:
-            self.value += line.split("\\")[0].strip()
-            if "\\" in line:
-                self.done = True
-
-
-class Time(Parser):
-    def __init__(self):
-        super(Time, self).__init__()
-        self.range = (-10, -1)
-
-    @is_done
-    def parse(self, line):
-        if 'Job cpu time' in line:
-            # " Job cpu time:  0 days  1 hours 24 minutes  3.8 seconds."
-            t = line.split()[3:][0::2]
-            con = (24., 1., 1 / 60., 1 / 3600)
-            self.value = str(sum(float(x) * con[i] for i, x in enumerate(t)))
-            self.done = True
-
-
-class Excited(Parser):
-    def __init__(self):
-        super(Excited, self).__init__()
-        self.range = (300, 3000)
-
-    @is_done
-    def parse(self, line):
-        # " Excited State   1:      Singlet-A      2.9126 eV  425.67 nm  f=0.7964  <S**2>=0.000"
-        if "Excited State   1:" in line:
-            self.value = line.split()[4]
-            self.done = True
-
-
-class Occupied(Parser):
-    def __init__(self):
-        super(Occupied, self).__init__()
-        self.prevline = ''
-        self.range = (-1000, -1)
-
-    @is_done
-    def parse(self, line):
-         # " Alpha  occ. eigenvalues --   -0.27354  -0.26346  -0.25649  -0.21987  -0.21885"
-         # " Alpha virt. eigenvalues --   -0.00138   0.03643   0.07104   0.08148   0.08460"
-        if "occ. eigenvalues" in line:
-            self.prevline = line
-        elif "virt. eigenvalues" in line and self.prevline:
-            self.value = str(float(self.prevline.split()[-1]) * 27.2117)
-            self.done = True
-
-
-class Virtual(Parser):
-    def __init__(self):
-        super(Virtual, self).__init__()
-        self.prevline = ''
-        self.range = (-1000, -1)
-
-    @is_done
-    def parse(self, line):
-         # " Alpha  occ. eigenvalues --   -0.27354  -0.26346  -0.25649  -0.21987  -0.21885"
-         # " Alpha virt. eigenvalues --   -0.00138   0.03643   0.07104   0.08148   0.08460"
-        if "occ. eigenvalues" in line:
-            self.prevline = line
-        elif "virt. eigenvalues" in line and self.prevline:
-            self.value = str(float(line.split()[4]) * 27.2117)
-            self.done = True
-
-
-class Geometry(Parser):
-    def __init__(self):
-        super(Geometry, self).__init__()
-        self.value = ''
-        self.start = False
-        self.range = (-300, -1)
-        self.newfile = True
-
-    @is_done
-    def parse(self, line):
-        # " -2012\0\\# opt b3lyp/6-31g(d) geom=connectivity iop(9/40=2)\\Title Car"
-        # " d Required\\0,1\C,-0.0013854631,-0.0120529361,-0.0064958728\C,-0.00021"
-        # ...
-        # " 4,1.2501547542\H,22.6120510229,1.0505502972,0.1022974384\H,2.283441615"
-        # " 6,-0.8632316482,20.4346296726\\Version=EM64L-G09RevC.01\State=1-A\HF=-"
-        if "\\" in line:
-            self.start = True
-        if self.start:
-            if r"\\@" in line:
-                start = self.value.index("#")
-                end =  self.value.index(r"\Version", start)
-
-                d = {",": " ", "\n ": "", "\\": "\n",
-                    "geom=connectivity": "",
-                }
-                value = self.value[start:end]
-                for i, j in d.iteritems():
-                    value = value.replace(i, j)
-                self.value = value
-                self.done = True
-            if not self.done:
-                self.value += line
-
-
-class Header(Parser):
-    def __init__(self):
-        super(Header, self).__init__()
-        self.value = ''
-        self.range = (70, 150)
-        self.start = False
-
-    @is_done
-    def parse(self, line):
-        # " %mem=59GB"
-        # " %chk=2_4g_TON_4g_4g_n4.chk"
-        # " --------------------------------------------------"
-        line = line.strip()
-        if "%mem" in line:
-            self.start = 1
-        if self.start and not self.done:
-            if line.startswith("%"):
-                self.value += line + "\n"
-            elif line.startswith("-"):
-                self.done = True
-
-class Dipole(Parser):
-    def __init__(self):
-        super(Dipole, self).__init__()
-        self.range = (-300, -1)
-
-    @is_done
-    def parse(self, line):
-        line = line.strip()
-        if line.startswith("X="):
-            self.value = line.split()[-1]
-            self.done = True
-
-##############################################################################
-##############################################################################
-
 class Log(object):
+    PARSERS = dict()
     def __init__(self, f, fname=None):
         self.f = f
         self.fname = fname if fname else f.name
         self.name, _ = os.path.splitext(self.fname)
         self.td = False
 
-        self.parsers = {
-            "HomoOrbital": HomoOrbital(),
-            "Energy": Energy(),
-            "Time": Time(),
-            "Occupied": Occupied(),
-            "Virtual": Virtual(),
-            "Dipole": Dipole(),
-            "Geometry": Geometry(),
-            "Header": Header(),
-            }
+        self.parsers = dict()
+        for k, v in Log.PARSERS.items():
+            self.parsers[k] = v()
+
         self.order = ["Occupied", "Virtual", "HomoOrbital", "Dipole", "Energy", "Time"]
         if self.name.lower().endswith("td") or self.name.lower().endswith("tddft"):
             self.parsers["Excited"] = Excited()
@@ -237,15 +34,15 @@ class Log(object):
             self.td = True
 
         possible = self.in_range()
-        if possible is not None:
-            for i, line in enumerate(f):
-                if i in possible:
-                    for k, parser in self.parsers.items():
-                        parser.parse(line)
-        else:
-            for i, line in enumerate(f):
+        for i, line in enumerate(f):
+            if possible is None or i in possible:
                 for k, parser in self.parsers.items():
                     parser.parse(line)
+
+    @classmethod
+    def add_parser(cls, parser):
+        cls.PARSERS[parser.__name__] = parser
+        return parser
 
     def in_range(self):
         '''Builds a set of line numbers based on parser params to optimally skip lines.'''
@@ -292,6 +89,214 @@ class Log(object):
 
     def format_header(self):
         return ', '.join(["Name"] + self.order)
+
+
+##############################################################################
+# Parsers
+##############################################################################
+
+def is_done(fn):
+    def wrapper(self, *args, **kwargs):
+        if not self.done:
+            return fn(self, *args, **kwargs)
+        else:
+            return self.value
+    return wrapper
+
+
+class Parser(object):
+    def __init__(self):
+        self.done = False
+        self.value = None
+        self.range = (0, 0)
+        self.newfile = False
+    def parse(self, line):
+        raise NotImplementedError
+    def __str__(self):
+        return str(self.value)
+    # def header(self):
+    #     return " ".join(re.split("([A-Z][^A-Z]*)", self.__class__.__name__)[1::2])
+
+##############################################################################
+##############################################################################
+
+@Log.add_parser
+class HomoOrbital(Parser):
+    def __init__(self):
+        super(HomoOrbital, self).__init__()
+        self.value = 0
+        self.range = (-1000, -1)
+
+    @is_done
+    def parse(self, line):
+        # " Alpha  occ. eigenvalues --  -88.90267 -19.16896 -19.16575 -19.15705 -19.15234"
+        # " Alpha  occ. eigenvalues --  -10.23876 -10.23843 -10.23775 -10.23715 -10.23374"
+        # ...
+        # " Alpha virt. eigenvalues --   -0.00138   0.03643   0.07104   0.08148   0.08460"
+        if "Alpha  occ. eigenvalues" in line:
+            self.value += len(line.split()[4:])
+        elif self.value:
+            self.value = str(self.value)
+            self.done = True
+
+
+@Log.add_parser
+class Energy(Parser):
+    def __init__(self):
+        super(Energy, self).__init__()
+        self.start = False
+        self.range = (-30, -1)
+
+    @is_done
+    def parse(self, line):
+        # " 36\\Version=EM64L-G09RevC.01\State=1-A\HF=-1127.8085512\RMSD=3.531e-09"
+        if "HF=" in line:
+            idx = line.index("HF=") + 3
+            self.value = line[idx:].split("\\")[0].strip()
+            self.start = True
+            if "\\" in line[idx:]:
+                self.done = True
+        elif self.start:
+            self.value += line.split("\\")[0].strip()
+            if "\\" in line:
+                self.done = True
+
+
+@Log.add_parser
+class Time(Parser):
+    def __init__(self):
+        super(Time, self).__init__()
+        self.range = (-10, -1)
+
+    @is_done
+    def parse(self, line):
+        if 'Job cpu time' in line:
+            # " Job cpu time:  0 days  1 hours 24 minutes  3.8 seconds."
+            t = line.split()[3:][0::2]
+            con = (24., 1., 1 / 60., 1 / 3600)
+            self.value = str(sum(float(x) * con[i] for i, x in enumerate(t)))
+            self.done = True
+
+
+@Log.add_parser
+class Excited(Parser):
+    def __init__(self):
+        super(Excited, self).__init__()
+        self.range = (300, 3000)
+
+    @is_done
+    def parse(self, line):
+        # " Excited State   1:      Singlet-A      2.9126 eV  425.67 nm  f=0.7964  <S**2>=0.000"
+        if "Excited State   1:" in line:
+            self.value = line.split()[4]
+            self.done = True
+
+
+@Log.add_parser
+class Occupied(Parser):
+    def __init__(self):
+        super(Occupied, self).__init__()
+        self.prevline = ''
+        self.range = (-1000, -1)
+
+    @is_done
+    def parse(self, line):
+         # " Alpha  occ. eigenvalues --   -0.27354  -0.26346  -0.25649  -0.21987  -0.21885"
+         # " Alpha virt. eigenvalues --   -0.00138   0.03643   0.07104   0.08148   0.08460"
+        if "occ. eigenvalues" in line:
+            self.prevline = line
+        elif "virt. eigenvalues" in line and self.prevline:
+            self.value = str(float(self.prevline.split()[-1]) * 27.2117)
+            self.done = True
+
+
+@Log.add_parser
+class Virtual(Parser):
+    def __init__(self):
+        super(Virtual, self).__init__()
+        self.prevline = ''
+        self.range = (-1000, -1)
+
+    @is_done
+    def parse(self, line):
+         # " Alpha  occ. eigenvalues --   -0.27354  -0.26346  -0.25649  -0.21987  -0.21885"
+         # " Alpha virt. eigenvalues --   -0.00138   0.03643   0.07104   0.08148   0.08460"
+        if "occ. eigenvalues" in line:
+            self.prevline = line
+        elif "virt. eigenvalues" in line and self.prevline:
+            self.value = str(float(line.split()[4]) * 27.2117)
+            self.done = True
+
+
+@Log.add_parser
+class Geometry(Parser):
+    def __init__(self):
+        super(Geometry, self).__init__()
+        self.value = ''
+        self.start = False
+        self.range = (-300, -1)
+        self.newfile = True
+
+    @is_done
+    def parse(self, line):
+        # " -2012\0\\# opt b3lyp/6-31g(d) geom=connectivity iop(9/40=2)\\Title Car"
+        # " d Required\\0,1\C,-0.0013854631,-0.0120529361,-0.0064958728\C,-0.00021"
+        # ...
+        # " 4,1.2501547542\H,22.6120510229,1.0505502972,0.1022974384\H,2.283441615"
+        # " 6,-0.8632316482,20.4346296726\\Version=EM64L-G09RevC.01\State=1-A\HF=-"
+        if "\\" in line:
+            self.start = True
+        if self.start:
+            if r"\\@" in line:
+                start = self.value.index("#")
+                end =  self.value.index(r"\Version", start)
+
+                d = {",": " ", "\n ": "", "\\": "\n",
+                    "geom=connectivity": "",
+                }
+                value = self.value[start:end]
+                for i, j in d.iteritems():
+                    value = value.replace(i, j)
+                self.value = value
+                self.done = True
+            if not self.done:
+                self.value += line
+
+
+@Log.add_parser
+class Header(Parser):
+    def __init__(self):
+        super(Header, self).__init__()
+        self.value = ''
+        self.range = (70, 150)
+        self.start = False
+
+    @is_done
+    def parse(self, line):
+        # " %mem=59GB"
+        # " %chk=2_4g_TON_4g_4g_n4.chk"
+        # " --------------------------------------------------"
+        line = line.strip()
+        if "%mem" in line:
+            self.start = 1
+        if self.start and not self.done:
+            if line.startswith("%"):
+                self.value += line + "\n"
+            elif line.startswith("-"):
+                self.done = True
+
+@Log.add_parser
+class Dipole(Parser):
+    def __init__(self):
+        super(Dipole, self).__init__()
+        self.range = (-300, -1)
+
+    @is_done
+    def parse(self, line):
+        line = line.strip()
+        if line.startswith("X="):
+            self.value = line.split()[-1]
+            self.done = True
 
 
 ##############################################################################
