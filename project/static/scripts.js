2d875f789a865bbad149306fd14e043b51ec7d14
chris0076 <crcoll9955@northgeorgia.edu>
2013-02-08 00:52:50 -0500

    Add format function to make making strings easier

--- project/static/scripts.js
+++ project/static/scripts.js
@@ -17,3 +17,12 @@ $(document).ready(function() {
         }
     });
 });
+
+String.prototype.format = function () {
+  var args = arguments;
+  return this.replace(/\{\{|\}\}|\{(\d+)\}/g, function (m, n) {
+    if (m == "{{") { return "{"; }
+    if (m == "}}") { return "}"; }
+    return args[n];
+  });
+};
