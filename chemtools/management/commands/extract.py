bcb3af11725f6780cf1c988423d84a60afd01f7d
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-10-03 00:33:33 -0400

    Add managment command to use the extractor

---
+++ chemtools/management/commands/extract.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand, CommandError
+from chemtools.extracter import run_all
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Extract mol2 data'
+
+    def handle(self, *args, **options):
+        run_all()
