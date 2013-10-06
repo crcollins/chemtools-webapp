46f8b3f543158a3cdaa456fc8636cd84719b8c5f
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-10-06 00:41:01 -0400

    Add tests for data app

---
+++ data/tests.py
@@ -0,0 +1,39 @@
+from itertools import product
+
+from django.test import Client, TestCase
+from django.core.urlresolvers import reverse
+
+import views
+from chemtools.utils import RGROUPS, ARYL, CLUSTERS
+
+class FragmentTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get(reverse(views.frag_index))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_ends(self):
+        for frag in RGROUPS + ARYL:
+            response = self.client.get(reverse(views.get_frag, args=(frag, )))
+            self.assertEqual(response.status_code, 200)
+
+    def test_detail_cores(self):
+        for frag in (''.join(x) for x in product("EZTC","234","34")):
+            response = self.client.get(reverse(views.get_frag, args=(frag, )))
+            self.assertEqual(response.status_code, 200)
+
+
+class TemplateTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get(reverse(views.template_index))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail(self):
+        for cluster in CLUSTERS:
+            response = self.client.get(reverse(views.get_template, args=(cluster, )))
+            self.assertEqual(response.status_code, 200)
