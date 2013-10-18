80cc210be8c6f90eb615b7d2d747b937d4e0c8a1
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-10-18 01:16:36 -0400

    Add test for parse app

---
+++ parse/tests.py
@@ -0,0 +1,12 @@
+from django.test import Client, TestCase
+from django.core.urlresolvers import reverse
+
+import views
+
+class MainPageTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get(reverse(views.upload_data))
+        self.assertEqual(response.status_code, 200)
