25d3f51ed79aa1a507b94021f388c71d7268088c
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-10-23 02:20:46 -0400

    Fix ajax modal

--- project/static/scripts.js
+++ project/static/scripts.js
@@ -7,13 +7,11 @@ $(document).ready(function() {
     });
 
     $("body").on({
-        // When ajaxStart is fired, add 'loading' to body class
         ajaxStart: function() {
-            $(this).addClass("loading");
+            $(".ajax-modal").fadeIn("slow");
         },
-        // When ajaxStop is fired, rmeove 'loading' from body class
         ajaxStop: function() {
-            $(this).removeClass("loading");
+            $(".ajax-modal").hide();
         }
     });
     $("select#id_cluster").change( function () {
