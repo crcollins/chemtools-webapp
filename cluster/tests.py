1f390dcb6631c78c1b482113ae61f5323429a5b6
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-10-30 15:38:47 -0400

    Add test get_job_list

--- cluster/tests.py
+++ cluster/tests.py
@@ -1,6 +1,7 @@
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+from django.utils import simplejson
 
 import views
 
@@ -18,3 +19,17 @@ class SSHPageTestCases(TestCase):
         self.assertTrue(r)
         response = self.client.get(reverse(views.job_index))
         self.assertEqual(response.status_code, 200)
+
+    def test_get_job_list(self):
+        response = self.client.get(reverse(views.get_job_list))
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_job_list_auth(self):
+        user = User.objects.get(username="testerman")
+        r = self.client.login(username="testerman", password="S0m3thing")
+        self.assertTrue(r)
+
+        response = self.client.get(reverse(views.get_job_list))
+        self.assertEqual(response.status_code, 200)
+        data = simplejson.loads(response.content)
+        self.assertTrue(data["is_authenticated"])
