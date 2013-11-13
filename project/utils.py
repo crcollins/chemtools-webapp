a296370f29d6264a685d3b56cbabd5cf01ddcb5d
Christopher Collins <crcoll9955@northgeorgia.edu>
2013-11-12 22:12:36 -0500

    Fix typo in password based sftp connection

--- project/utils.py
+++ project/utils.py
@@ -53,7 +53,7 @@ def get_sftp_connection(hostname, username, key=None, password=None, port=22):
         pkey = paramiko.RSAKey.from_private_key(key)
         transport.connect(username=username, pkey=pkey)
     else:
-        transport.connect(username=username, password=pkey)
+        transport.connect(username=username, password=password)
     return SFTPClient.from_transport(transport)
 
 def get_ssh_connection(hostname, username, key=None, password=None, port=22):
