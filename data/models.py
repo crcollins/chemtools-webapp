8581fae9a70571ed19a97078b7de87eeae8b7033
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-01-26 22:49:56 -0500

    Add model to store log data

---
+++ data/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class DataPoint(models.Model):
+    name = models.CharField(max_length=200)
+    exact_name = models.CharField(max_length=600, null=True, blank=True)
+
+    options = models.CharField(max_length=100)
+    occupied = models.FloatField()
+    virtual = models.FloatField()
+    homo_orbital = models.IntegerField()
+    energy = models.FloatField()
+    dipole = models.FloatField()
+    band_gap = models.FloatField(null=True, blank=True)
