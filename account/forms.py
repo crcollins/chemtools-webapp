6d6d1af248ce555cca56521bba5e7c356817c74e
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-01-28 21:48:23 -0500

    Remove unused section of SettingsForm

--- account/forms.py
+++ account/forms.py
@@ -32,12 +32,6 @@ class SettingsForm(forms.Form):
                                 label="XSEDE Username")
     new_ssh_keypair = forms.BooleanField(required=False)
 
-    def clean(self):
-        if "password1" in self.cleaned_data and "password2" in self.cleaned_data:
-            if self.cleaned_data["password1"] != self.cleaned_data["password2"]:
-                raise forms.ValidationError("The two password fields did not match.")
-        return self.cleaned_data
-
 
 class UserProfileForm(forms.ModelForm):
     private_key = forms.CharField(widget=forms.Textarea)
