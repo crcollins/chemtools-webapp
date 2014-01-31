8255ad82db7fb77f3ae4e1400434582d777febb9
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-01-31 00:02:12 -0500

    Add tests for DataPoint

--- data/tests.py
+++ data/tests.py
@@ -4,6 +4,7 @@ from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 
 import views
+import models
 from chemtools.utils import RGROUPS, XGROUPS, ARYL, CLUSTERS
 
 
@@ -38,3 +39,16 @@ class TemplateTestCase(TestCase):
         for cluster in CLUSTERS:
             response = self.client.get(reverse(views.get_template, args=(cluster, )))
             self.assertEqual(response.status_code, 200)
+
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        new_data = models.DataPoint(name="A_TON_A_A", exact_name="A_TON_A_A_n1_m1_x1_y1_z1",
+                    options="td B3LYP/6-31g(d) geom=connectivity", homo=-6.460873931,
+                    lumo=-1.31976745, homo_orbital=41, dipole=0.0006,
+                    energy=-567.1965205, band_gap=4.8068)
+        new_data.save()
+
+    def test_datapoint_unicode(self):
+        string = str(models.DataPoint.objects.all()[0])
+        self.assertEqual(string, "A_TON_A_A_n1_m1_x1_y1_z1")
