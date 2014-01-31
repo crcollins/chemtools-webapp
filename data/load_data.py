a6b1e12014c34fbaa2a64abd2d51122d3525a66a
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-01-31 01:15:18 -0500

    Move load_data into a function

--- data/load_data.py
+++ data/load_data.py
@@ -4,35 +4,34 @@ import os
 from chemtools.mol_name import get_exact_name
 from models import DataPoint
 
-folder, _ = os.path.split(__file__)
-PATH = os.path.join(folder, "data.csv")
-with open(PATH, "r") as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    points = []
-    for row in reader:
-        if row == []:
-            continue
-        try:
-            band_gap = row[10]
-            if band_gap == '---':
-                band_gap = None
-            options = row[4]
-
+def main():
+    folder, _ = os.path.split(__file__)
+    PATH = os.path.join(folder, "data.csv")
+    with open(PATH, "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        points = []
+        for row in reader:
+            if row == []:
+                continue
             try:
-                exact_name = get_exact_name(row[1])
-            except:
-                exact_name = None
+                band_gap = row[10]
+                if band_gap == '---':
+                    band_gap = None
+                options = row[4]
 
-            print row[1]
-            point = DataPoint(
-                    name=row[1], options=row[4],
-                    homo=row[5], lumo=row[6],
-                    homo_orbital=row[7], dipole=row[8],
-                    energy=row[9], band_gap=band_gap,
-                    exact_name=exact_name)
-            point.clean_fields()
-            points.append(point)
-        except Exception as e:
-            print e
-    DataPoint.objects.bulk_create(points)
+                try:
+                    exact_name = get_exact_name(row[1])
+                except:
+                    exact_name = None
 
+                point = DataPoint(
+                        name=row[1], options=row[4],
+                        homo=row[5], lumo=row[6],
+                        homo_orbital=row[7], dipole=row[8],
+                        energy=row[9], band_gap=band_gap,
+                        exact_name=exact_name)
+                point.clean_fields()
+                points.append(point)
+            except Exception as e:
+                pass
+        DataPoint.objects.bulk_create(points)
