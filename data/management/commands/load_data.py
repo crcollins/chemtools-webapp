b3d64f6b95f58270979e8bbe0ad62febd4c06914
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-02-08 13:47:54 -0500

    Add management command for load_data

---
+++ data/management/commands/load_data.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand, CommandError
+from data.load_data import main
+
+class Command(BaseCommand):
+    args = 'path'
+    help = 'Load parsed data into the database'
+
+    def handle(self, path, *args, **options):
+        main(path)
