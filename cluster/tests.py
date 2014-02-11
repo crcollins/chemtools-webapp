66005f371ad4d8e58a8fcd1cc30140983b8b6596
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-02-10 21:21:44 -0500

    Add tests for getting ssh/sftp connections

--- cluster/tests.py
+++ cluster/tests.py
@@ -1,3 +1,6 @@
+import os
+
+from django.conf import settings
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
@@ -37,8 +40,37 @@ class SSHPageTestCases(TestCase):
         self.assertTrue(data["is_authenticated"])
 
 
-
 class UtilsTestCase(TestCase):
+    def test_get_sftp_password(self):
+        sftp = get_sftp_connection("localhost", "vagrant", password="vagrant", port=2222)
+        with sftp:
+            pass
+
+    def test_get_sftp_key(self):
+        with open(os.path.join(settings.MEDIA_ROOT, "tests", "id_rsa"), 'r') as key:
+            sftp = get_sftp_connection("localhost", "vagrant", key=key, port=2222)
+            with sftp:
+                pass
+
+    def test_get_sftp_error(self):
+        with self.assertRaises(Exception):
+            get_sftp_connection("localhost", "username")
+
+    def test_get_ssh_password(self):
+        ssh = get_ssh_connection("localhost", "vagrant", password="vagrant", port=2222)
+        with ssh:
+            pass
+
+    def test_get_ssh_key(self):
+        with open(os.path.join(settings.MEDIA_ROOT, "tests", "id_rsa"), 'r') as key:
+            ssh = get_ssh_connection("localhost", "vagrant", key=key, port=2222)
+            with ssh:
+                pass
+
+    def test_get_ssh_error(self):
+        with self.assertRaises(Exception):
+            get_ssh_connection("localhost", "username")
+
     def test_AES(self):
         cipher = AESCipher()
         string = "The quick brown fox jumps over the lazy dog."
