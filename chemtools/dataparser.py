e8b248e799b7296283f62380486eaebd374cb00d
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-03-01 21:00:46 -0500

    Update dataparser so that it is a little easier to use with an API
    This change moves the curve fitting into a separate function so it
    can be used in other parts of the code.
    
    In addition, there is some simplification in writing the output.
    This change adds an extra line before the error messages. This is
    why there is the added line in output.txt

--- chemtools/dataparser.py
+++ chemtools/dataparser.py
@@ -11,6 +11,44 @@ np.seterr(all="ignore")
 from utils import Output, catch
 
 
+def kuhn_exp(x, a, b):
+    return a * np.sqrt(1 - b * np.cos(math.pi / (x + 1)))
+
+
+def predict_values(xvals, homovals, lumovals, gapvals):
+    x = np.array(xvals)
+    maxx = max(xvals)
+    if maxx > 1:
+        x = 1. / x
+        maxx = x.max()
+
+    homoy = np.array(homovals)
+    homo_fit = lambda x, a, b: kuhn_exp(x, a, b)
+    (homoa, homob), var_matrix = curve_fit(homo_fit, x, homoy, p0=[-8, -.8])
+    homo_func = lambda x: kuhn_exp(x, homoa, homob)
+
+    lumoy = np.array(lumovals)
+    lumo_fit = lambda x, a, b: kuhn_exp(x, a, b) + homo_func(x)
+    (lumoa, lumob), var_matrix = curve_fit(lumo_fit, x, lumoy, p0=[5, -.8])
+    lumo_func = lambda x: kuhn_exp(x, lumoa, lumob) + homo_func(x)
+
+    gapy = np.array(gapvals)
+    gap_fit = lambda x, a, b: kuhn_exp(x, a, b) + lumo_func(x)
+    (gapa, gapb), var_matrix = curve_fit(gap_fit, x, gapy, p0=[11, -.8])
+    gap_func = lambda x: kuhn_exp(x, gapa, gapb) + lumo_func(x)
+
+    homo_limit = homo_func(0)
+    lumo_limit = lumo_func(0)
+    gap_limit = gap_func(0)
+
+    results = {
+        "homo": (homo_limit, homoa, homob, homo_func),
+        "lumo": (lumo_limit, lumoa, lumob, lumo_func),
+        "gap": (gap_limit, gapa, gapb, gap_func),
+    }
+    return results
+
+
 class DataParser(Output):
     def __init__(self, f):
         super(DataParser, self).__init__()
@@ -29,51 +67,42 @@ class DataParser(Output):
 
     @catch
     def parse_file(self, f):
-        def homofunc(x, a, b):
-            return a * np.sqrt(1 - b * np.cos(math.pi / (x + 1)))
-
         datax, datahomo, datalumo, datagap = self.extract_data(f)
-
         x = np.array(datax)
+        homoy = np.array(datahomo)
+        lumoy = np.array(datalumo)
+        gapy = np.array(datagap)
+        results = predict_values(datax, homoy, lumoy, gapy)
+
+        for key in ["Homo", "Lumo", "Gap"]:
+            values = results[key.lower()]
+            self.write(key)
+            self.write("A: %f, B: %f" % (values[1], values[2]))
+            self.write("limit: %f" % values[0])
+            self.write('')
+
         maxx = max(datax)
         if maxx > 1:
             x = 1. / x
             maxx = x.max()
+        xvals = np.linspace(0, maxx, 20)
 
-        homoy = np.array(datahomo)
-        (homoa, homob), var_matrix = curve_fit(homofunc, x, homoy, p0=[-8, -.8])
-        self.write("Homo")
-        self.write("A: %f, B: %f" % (homoa, homob))
-        self.write("limit: %f" % homofunc(0, homoa, homob))
-        self.write("")
-
-        lumofunc = lambda x, a, b: homofunc(x, a, b) + homofunc(x, homoa, homob)
-        lumoy = np.array(datalumo)
-        (lumoa, lumob), var_matrix = curve_fit(lumofunc, x, lumoy, p0=[5, -.8])
-        self.write("Lumo")
-        self.write("A: %f, B: %f" % (lumoa, lumob))
-        self.write("limit: %f" % lumofunc(0, lumoa, lumob))
-        self.write("")
-
-        gapfunc = lambda x, a, b: homofunc(x, a, b) + lumofunc(x, lumoa, lumob)
-        gapy = np.array(datagap)
-        (gapa, gapb), var_matrix = curve_fit(gapfunc, x, gapy, p0=[11, -.8])
-        self.write("Gap")
-        self.write("A: %f, B: %f" % (gapa, gapb))
-        self.write("limit: %f" % gapfunc(0, gapa, gapb))
-
+        # Make HOMO/LUMO plot
         plot.plot(x, homoy, 'ro')
-        plot.plot(np.linspace(0, maxx, 20), homofunc(np.linspace(0, maxx, 20), homoa, homob), 'r')
+        homo_func = results["homo"][3]
+        plot.plot(xvals, homo_func(xvals), 'r')
         plot.plot(x, lumoy, 'ro')
-        plot.plot(np.linspace(0, maxx, 20), lumofunc(np.linspace(0, maxx, 20), lumoa, lumob), 'g')
-
+        lumo_func = results["lumo"][3]
+        plot.plot(xvals, lumo_func(xvals), 'g')
         plot.ylabel("Eg in eV")
         plot.xlabel("1/N")
         plot.savefig(self.plots[0], format="eps")
         plot.clf()
 
+        # Make Gap plot
         plot.plot(x, gapy, 'ro')
-        plot.plot(np.linspace(0, maxx, 20), gapfunc(np.linspace(0, maxx, 20), gapa, gapb), 'r')
+        gap_func = results["gap"][3]
+        plot.plot(xvals, gap_func(xvals), 'r')
         plot.ylabel("Eg in eV")
         plot.xlabel("1/N")
         plot.savefig(self.plots[1], format="eps")
