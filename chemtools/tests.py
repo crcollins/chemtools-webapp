1106cabefa5af91638aa32670fa73bd096980173
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-06-21 06:08:31 -0400

    Add tests for z matrix functions

--- chemtools/tests.py
+++ chemtools/tests.py
@@ -661,9 +661,111 @@ class FileParserTestCase(TestCase):
         expected = "\n---- Errors (1) ----\nValueError('some string',)\n"
         self.assertEqual(test.format_output(errors=True), expected)
 
+METHANE = """
+ C
+ H                  1            B1
+ H                  1            B2    2            A1
+ H                  1            B3    3            A2    2            D1    0
+ H                  1            B4    3            A3    2            D2    0
+
+   B1             1.07000000
+   B2             1.07000000
+   B3             1.07000000
+   B4             1.07000000
+   A1           109.47120255
+   A2           109.47125080
+   A3           109.47121829
+   D1          -119.99998525
+   D2           120.00000060
+"""
+METHANE_REPLACED = """
+ C
+ H                  1            1.07000000
+ H                  1            1.07000000    2            109.47120255
+ H                  1            1.07000000    3            109.47125080    2            -119.99998525    0
+ H                  1            1.07000000    3            109.47121829    2            120.00000060    0
+"""
+METHANE_CART = """
+ C 0.00000000 0.00000000 0.00000000
+ H 1.07000000 0.00000000 0.00000000
+ H -0.35666635 -1.00880579 0.00000000
+ H -0.35666635 0.50440312 -0.87365131
+ H -0.35666686 0.50440269 0.87365135
+"""
+BENZENE = """
+ C
+ C                  1            B1
+ C                  2            B2    1            A1
+ C                  3            B3    2            A2    1            D1    0
+ C                  4            B4    3            A3    2            D2    0
+ C                  1            B5    2            A4    3            D3    0
+ H                  1            B6    6            A5    5            D4    0
+ H                  2            B7    1            A6    6            D5    0
+ H                  3            B8    2            A7    1            D6    0
+ H                  4            B9    3            A8    2            D7    0
+ H                  5           B10    4            A9    3            D8    0
+ H                  6           B11    1           A10    2            D9    0
+
+   B1             1.39516000
+   B2             1.39471206
+   B3             1.39542701
+   B4             1.39482508
+   B5             1.39482907
+   B6             1.09961031
+   B7             1.09965530
+   B8             1.09968019
+   B9             1.09968011
+   B10            1.09976099
+   B11            1.09960403
+   A1           120.00863221
+   A2           119.99416459
+   A3           119.99399231
+   A4           119.99845680
+   A5           120.00431986
+   A6           119.98077039
+   A7           120.01279489
+   A8           119.98114211
+   A9           120.01134336
+   A10          120.00799702
+   D1            -0.05684321
+   D2             0.03411439
+   D3             0.03234809
+   D4          -179.97984142
+   D5           179.95324796
+   D6           179.96185208
+   D7          -179.99643617
+   D8          -179.99951388
+   D9           179.98917535
+   """
+BENZENE_CART = """
+ C 0.00000000 0.00000000 0.00000000
+ C 1.39516000 0.00000000 0.00000000
+ C 2.09269800 1.20775100 0.00000000
+ C 1.39504400 2.41626000 0.00119900
+ C 0.00022024 2.41618509 -0.00071869
+ C -0.69738200 1.20797600 0.00068200
+ H -0.54975823 -0.95231577 -0.00184417
+ H 1.94466800 -0.95251300 -0.00131500
+ H 3.19237800 1.20783100 -0.00063400
+ H 1.94524390 3.36840107 0.00314780
+ H -0.54990178 3.36846608 0.00023365
+ H -1.79698600 1.20815900 0.00086200
+   """
 
 class UtilsTestCase(TestCase):
-    pass
+    def test_replace_geom_vars(self):
+        results = utils.replace_geom_vars(METHANE.strip())
+        self.assertEqual(METHANE_REPLACED.strip(), results)
+
+    def test_convert_zmatrix_to_cart_meth(self):
+        string = utils.replace_geom_vars(METHANE.strip())
+        results = utils.convert_zmatrix_to_cart(string)
+        self.assertEqual(METHANE_CART.strip(), results.strip())
+
+    def test_convert_zmatrix_to_cart_benz(self):
+        string = utils.replace_geom_vars(BENZENE.strip())
+        results = utils.convert_zmatrix_to_cart(string)
+        self.assertEqual(BENZENE_CART.strip(), results.strip())
 
 
 class GraphTestCase(TestCase):
