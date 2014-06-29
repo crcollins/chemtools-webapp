af2d1b1ae6c51965ca89b57a71b300f47f2c8668
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-06-28 21:58:28 -0400

    Add feature vectors and predictors to the admin page

--- data/admin.py
+++ data/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
-from models import DataPoint
+from models import DataPoint, FeatureVector, Predictor
 
 
 class DataPointAdmin(admin.ModelAdmin):
     list_display = ("name", "exact_name", "options", "band_gap")
 
 
+class FeatureVectorAdmin(admin.ModelAdmin):
+    list_display = ("datapoint", "type")
+
+
+class PredictorAdmin(admin.ModelAdmin):
+    list_display = ("pickle", "homo_error", "lumo_error", "gap_error", "created")
+
+
 admin.site.register(DataPoint, DataPointAdmin)
+admin.site.register(FeatureVector, FeatureVectorAdmin)
+admin.site.register(Predictor, PredictorAdmin)
