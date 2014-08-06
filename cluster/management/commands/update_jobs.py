51d021b539e750568a8047ff45799fdd4533cc67
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-08-06 10:02:14 -0400

    Add management command for update_jobs

---
+++ cluster/management/commands/update_jobs.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from cluster.update_jobs import run_all
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Update Job data from clusters'
+
+    def handle(self, *args, **options):
+        run_all()
