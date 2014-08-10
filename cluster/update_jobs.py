634ae735db61ebb211b9e3159ca4dac7861e5553
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-08-10 00:00:17 -0400

    Add updating of jobs if their state is now unknown

--- cluster/update_jobs.py
+++ cluster/update_jobs.py
@@ -10,13 +10,19 @@ def run_all():
         for i, cluster in enumerate(get_all_jobs(user)):
             cred = creds[i]
             jobs = {}
+            jobids = []
             for job in cluster["jobs"]:
                 status = job[-1]
                 job_id = job[0]
+                jobids.append(job_id)
                 if status in jobs:
                     jobs[status].append(job_id)
                 else:
                     jobs[status] = [job_id]
+            running = Job.get_running_jobs(credential=cred)
+            unknown = running.exclude(jobid__in=set(jobids)).values_list('jobid', flat=True)
+            if unknown:
+                jobs[Job.UNKNOWN] = list(unknown)
             Job.update_states(cred, jobs)
 
 
