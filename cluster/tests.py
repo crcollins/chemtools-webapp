8ce77da733b6478daa79b73566a462decd3f3905
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-09-11 08:15:55 -0400

    Add test case for fileparser error in get_log_data

--- cluster/tests.py
+++ cluster/tests.py
@@ -698,6 +698,19 @@ class InterfaceTestCase(TestCase):
         results = interface.get_log_data(self.credential2, [""])
         self.assertIn("IndexError", results["error"])
 
+    def test_get_log_data_argparse(self):
+        with self.credential2.get_sftp_connection() as sftp:
+            with sftp.open("chemtools/fileparser.py", 'w') as f:
+                f.write("from __future__ import print_function\n")
+                f.write("import sys\n")
+                f.write("print('No module named argparse', file=sys.stderr)")
+
+        results = interface.get_log_data(self.credential2, [""])
+        self.assertIn("No module named argparse", results["error"])
+
+        with self.credential2.get_ssh_connection() as ssh:
+            ssh.exec_command("rm chemtools/fileparser.py")
+
     def test_get_log_data_invalid_credential(self):
         results = interface.get_log_data(None, "24a_TON")
         self.assertEqual(results["error"], CRED_ERROR)
