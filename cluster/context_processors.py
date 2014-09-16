9d70dc1f82fb807c02f4ccfa04bef7f6da36cbc6
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-09-16 12:16:47 -0400

    Add a hack so that the number of jobs running will update correctly

--- cluster/context_processors.py
+++ cluster/context_processors.py
@@ -1,8 +1,11 @@
 from models import Job
+from interface import get_all_jobs
 
 
 def running_jobs(request):
     if request.user.is_authenticated():
+        # hack to get numbers to update
+        get_all_jobs(request.user)
         temp = len(Job.get_running_jobs(user=request.user))
         return {"running_jobs": temp}
     else:
