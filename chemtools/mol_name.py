e5a4b7f0edecc9974047321441ee7a20b1dd441f
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-11-15 23:36:01 -0500

    Comment out the mol_name repeating stuff

--- chemtools/mol_name.py
+++ chemtools/mol_name.py
@@ -340,10 +340,10 @@ def get_exact_name(name, spacers=False):
                 # [char, conn, flip?]
                 endname = ''.join([x[0] + '-' if x[2] else x[0] for x in end])
 
-                if core is None:
-                    endname, num_repeats = find_repeating(tokenize(endname))
-                    endname = ''.join(endname)
-                    nm = max(nm[0], 1) * num_repeats, nm[1]
+                # if core is None:
+                #     endname, num_repeats = find_repeating(tokenize(endname))
+                #     endname = ''.join(endname)
+                #     nm = max(nm[0], 1) * num_repeats, nm[1]
 
             if not endname or endname[-1] not in XGROUPS:
                 if f(num):
