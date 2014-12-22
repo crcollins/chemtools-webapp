98335002f4304c7af0978fd26ab187dcc6dae12d
Christopher Collins <crcoll9955@northgeorgia.edu>
2014-12-21 20:07:12 -0500

    Add comments to the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Allow setting which user to install with and where chemtools will install to.
 [ -z "$INSTALL_USER" ] && INSTALL_USER=$USER
 [ -z "$CHEMTOOLS_DIR" ] && CHEMTOOLS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PIP_DEFAULT_TIMEOUT=600
@@ -16,8 +17,10 @@ install_chemtools() {
     cd $CHEMTOOLS_DIR
     virtualenv .
     . bin/activate
+    # This is needed because pycairo is not available in pip
     ln -fs /usr/lib/python2.7/dist-packages/cairo/ lib/python2.7/site-packages/
 
+    # Numpy needs to install before the others because sometimes it has install issues
     pip install numpy==1.6.1
     pip install -r requirements.txt
     python manage.py syncdb --noinput
@@ -30,10 +33,13 @@ setup_nginx() {
     cd $CHEMTOOLS_DIR
     . bin/activate
     pip install gunicorn
+
+    # This is to setup the periodic update of the ML models
     sudo tee /etc/cron.d/chemtools <<EOF
 PATH=$CHEMTOOLS_DIR/bin
 0 3 * * * $INSTALL_USER cd $CHEMTOOLS_DIR && python -u $CHEMTOOLS_DIR/manage.py update_ml >> $CHEMTOOLS_DIR/ml_update.log
 EOF
+    # Select the usage of the HTTPS configuration
     if [ -z "$HTTPS" ];
         then
         NGINX_SETTINGS=nginx_settings.conf
