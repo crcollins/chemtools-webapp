abcba823f7de49b9fc9c89f0e971d524b4f1034f
Christopher Collins <crcoll9955@northgeorgia.edu>
2015-03-27 17:05:27 -0400

    Add the initial framework for the template settings page

--- data/views.py
+++ data/views.py
@@ -3,9 +3,12 @@ import os
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.servers.basehttp import FileWrapper
+from django.contrib.auth.decorators import login_required
 
 from chemtools.extractor import CORES, RGROUPS, ARYL
 from data.models import JobTemplate
+from data.forms import JobTemplateForm
+from account.utils import add_account_page, PAGES
 
 
 def frag_index(request):
@@ -35,3 +38,15 @@ def get_frag(request, frag):
 def template_index(request):
     c = {"templates": JobTemplate.objects.all()}
     return render(request, "data/template_index.html", c)
+
+
+@login_required
+@add_account_page("templates")
+def template_settings(request, username):
+    c = {
+        "pages": PAGES,
+        "page": "templates",
+        "templates": JobTemplate.get_templates(request.user),
+        "form": JobTemplateForm(),
+    }
+    return render(request, "data/template_settings.html", c)
