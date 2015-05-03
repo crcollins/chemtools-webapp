53ff351f0f82393e5c1e72d6eb19f47e71517622
Christopher Collins <crcoll9955@northgeorgia.edu>
2015-05-02 20:50:26 -0400

    Make the apt-get install quiet

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ export PIP_DEFAULT_TIMEOUT=600
 
 dependencies() {
     sudo apt-get update
-    sudo apt-get install -y git python2.7 python-dev gfortran liblapack-dev\
+    sudo apt-get install -y -qq git python2.7 python-dev gfortran liblapack-dev\
                             libatlas-dev build-essential libfreetype6-dev\
                             libpng-dev python-cairo python-pip
     sudo pip install virtualenv
